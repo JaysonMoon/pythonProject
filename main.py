8c35d8625cd4ad25eb326c880fba1096b356edae
JaysonM <63750148+JaysonMoon@users.noreply.github.com>
2020-11-20 11:46:54 -0500

    Init

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+total_days = []
+for _ in range(1000):
+    infection_chance = .1
+    total_computers = [0] * 99
+    total_computers.append(1)
+    repair_rate = 10
+    days = 0
 
+    probabilities = []  # create a list of random probability values
+    for _ in range(100):
+        probabilities.append(random.uniform(0, 1))  # random values between 0 and 1 inclusive
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    while True:
+        for i in range(100):
+            if probabilities[i] > infection_chance:
+                total_computers[i] = 1
+                infection_chance = 1 - (.9 ** sum(total_computers))
+                days += 1
+                for j in range(100):  # technician will repair 10 comps by going down the line
+                    count = 0
+                    if total_computers[j] == 1:
+                        total_computers[j] = 0
+                        count += 1
+                    if count == 10:  # technician can only repair first ten comps
+                        break
+        if sum(total_computers) < 11:
+            total_days.append(days + 1)
+            break
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f"After 1000 trials, the average number of days it takes to fully repair the computers is {sum(total_days) / 1000} days")
